2fe39be466d59786c1a462c93819a7fd6b798a21
Juan <juan@wmedia.,es>
2019-11-12 13:22:17 +0100

    Configurando Vuex

--- src/store.js
+++ src/store.js
@@ -1,16 +1,31 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import firebase from './firebase.js';
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
+const store = new Vuex.Store({
   state: {
-
+    entries: null,
+    user: null,
+    userProfile: {},
   },
   mutations: {
-
+    loadEntries(state, val) {
+      state.entries = val;
+    },
+    saveUser(state, val) {
+      state.user = val;
+    },
+    saveUserProfile(state, val) {
+      state.userProfile = val;
+    }
   },
   actions: {
+    getUserProfile({ commit, sate }) {
 
+    }
   }
 })
+
+export default store;
